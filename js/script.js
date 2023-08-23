48f4873ab877b085019dcfe4e8f82d62dcd53615
Pablo Dettori <106544349+p4bloxx@users.noreply.github.com>
2023-08-23 19:26:23 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const currYears = date.getFullYear(); //return current year (format 2XXX)
 const currMonth = date.getMonth() + 1; //return current month, adjusted with + 1
 const currDay = date.getDate(); //return current day
 
+
 //Components
 const day = document.getElementById('day');
 const month = document.getElementById('month');
@@ -24,141 +25,40 @@ const resMonth = document.getElementById('resMonth');
 const resDay = document.getElementById('resDay');
 let form = document.getElementById('form');
 
-
-// to calculate age with user's input values
-function calcAge () {
+//function to check if input values are correct
+function checkInput () { 
   form.addEventListener('input', (e) => {
-    if (e.target === day && day.value !== 0) {
-       
-      if(day.value.length < 1 || day.value.length > 2) {
+     
+    //check if day'values are valid or empty
+      if(day.value.length > 2 || day.value > 31) {
+  
         day.classList.add('active-inp');
         labDay.classList.add('active-lab');
         errDay.classList.add('active-err');
+        errDay.innerText = 'Must be a valid day';
         resDay.innerText = '--';
 
+        checkDay = false;
+        
+      } else if (day.value < 1) {
+        day.classList.add('active-inp');
+        labDay.classList.add('active-lab');
+        errDay.classList.add('active-err');
+        errDay.innerText = 'This field is required';
+
+        checkDay = false;
+  
       }  else {
         day.classList.remove('active-inp');
         labDay.classList.remove('active-lab');
         errDay.classList.remove('active-err');
-        resDay.innerText = '--';
-      } 
+
+        checkDay = true;
+      }
+      
+      //check if year is leap or not
+      if (!(years.value % 400 == 0 || years.value % 4 == 0 && years.value % 100 != 0) && month.value == 2 && day.value == 29) {
   
-    }  else if (e.target === month && month.value !== 0) {
-      
-        if(month.value.length < 1 || month.value.length > 2) {
-          month.classList.add('active-inp');
-          labMonth.classList.add('active-lab');
-          errMonth.classList.add('active-err');
-          resMonth.innerText = '--';
-
-        }  else {
-          month.classList.remove('active-inp');
-          labMonth.classList.remove('active-lab');
-          errMonth.classList.remove('active-err');
-          resMonth.innerText = '--';
-        }
-      
-    } else if (e.target === years && years.value !== 0) {
-  
-        if (years.value > currYears) {
-          years.classList.add('active-inp');
-          labYears.classList.add('active-lab');
-          errYears.classList.add('active-err');
-          errYears.innerText = 'Must be in the past';
-          resYears.innerText = '--';
-          
-        }  else {
-          years.classList.remove('active-inp');
-          labYears.classList.remove('active-lab');
-          errYears.classList.remove('active-err');
-        }
-     }
-     
-  });
-
-    let resultDay = day.value;
-    let resultMonth = month.value;
-    let resultYears = years.value;
-
-    //check if year is Bisestile or not
-    if (!(resultYears % 400 == 0 || resultYears % 4 == 0 && resultYears % 100 != 0) && resultMonth == 2 && resultDay == 29) {
-      console.log('ciao a tutti')
-      day.classList.add('active-inp');
-      labDay.classList.add('active-lab');
-      errDay.classList.add('active-err');
-      errDay.innerText = 'Must be a valid day';
-
-      resDay.innerText = '--';
-      resMonth.innerText = '--';
-      resYears.innerText = '--';
-      
-    } else {
-      day.classList.remove('active-inp');
-      labDay.classList.remove('active-lab');
-      errDay.classList.remove('active-err');
-    }
-
-    //check if different parameters are respected
-
-    //if input values are empty
-    if(resultDay === '' && resultMonth === '' && resultYears === '') {
-
-      day.classList.add('active-inp');
-      labDay.classList.add('active-lab');
-      errDay.classList.add('active-err');
-      errDay.innerText = 'This field is required'
-
-      month.classList.add('active-inp');
-      labMonth.classList.add('active-lab');
-      errMonth.classList.add('active-err');
-      errMonth.innerText = 'This field is required'
-
-      years.classList.add('active-inp');
-      labYears.classList.add('active-lab');
-      errYears.classList.add('active-err');
-      errYears.innerText = 'This field is required'
-
-      resDay.innerText = '--';
-      resMonth.innerText = '--';
-      resYears.innerText = '--';
-
-      //if number of days are correctly
-    } else if(resultDay == 0 || resultDay > 31) {
-
-      day.classList.add('active-inp');
-      labDay.classList.add('active-lab');
-      errDay.classList.add('active-err');
-      errDay.innerText = 'Must be a valid day'
-
-      resDay.innerText = '--';
-
-      //if number of months are correctly
-    } else if (resultMonth > 12 || resultMonth == 0) {
-
-      month.classList.add('active-inp');
-      labMonth.classList.add('active-lab');
-      errMonth.classList.add('active-err');
-      errMonth.innerText = 'Must be a valid month'
-
-      resDay.innerText = '--';
-      resMonth.innerText = '--';
-      resYears.innerText = '--';
-    
-      //if number of days are correctly with months' end 31
-    } else if ((resultMonth == 4 || resultMonth == 6 || resultMonth == 9 || resultMonth == 11) && resultDay > 30) {
-
-      day.classList.add('active-inp');
-      labDay.classList.add('active-lab');
-      errDay.classList.add('active-err');
-      errDay.innerText = 'Must be a valid day';
-
-      resDay.innerText = '--';
-      resMonth.innerText = '--';
-      resYears.innerText = '--';
-
-      //if number of days are correctly february
-    } else if (resultMonth == 2 && resultDay > 29) {
-
         day.classList.add('active-inp');
         labDay.classList.add('active-lab');
         errDay.classList.add('active-err');
@@ -167,67 +67,233 @@ function calcAge () {
         resDay.innerText = '--';
         resMonth.innerText = '--';
         resYears.innerText = '--';
+
+        checkDay = false;
+  
+        //check if year's value are more high than current year
+      } else  if (years.value > currYears) {
+        years.classList.add('active-inp');
+        labYears.classList.add('active-lab');
+        errYears.classList.add('active-err');
+        errYears.innerText = 'Must be in the past';
+
+        resYears.innerText = '--';
+        checkYears = false
+        
+      } else if (years.value < 1) {
+  
+        years.classList.add('active-inp');
+        labYears.classList.add('active-lab');
+        errYears.classList.add('active-err');
+        errYears.innerText = 'This field is required';
+        checkYears = false
+  
+      }  else if (years.value.length !== 4) {
+        years.classList.add('active-inp');
+        labYears.classList.add('active-lab');
+        errYears.classList.add('active-err');
+        errYears.innerText = 'Must be a valid year';
+        checkYears = false
+  
+        resYears.innerText = '--';
+        resDay.innerText = '--';
+        resMonth.innerText = '--';
+  
+      } else {
+  
+        years.classList.remove('active-inp');
+        labYears.classList.remove('active-lab');
+        errYears.classList.remove('active-err');
+
+        checkYears = true;
+      } 
+  
+      //check if number of month is correct
+      if (month.value.length > 2 || month.value > 12) {
+  
+        month.classList.add('active-inp');
+        labMonth.classList.add('active-lab');
+        errMonth.classList.add('active-err');
+        errMonth.innerText = 'Must be a valid month';
+        checkMonth = false;
+  
+        //particularly in february
+      } else if (month.value == 2 && day.value > 29) {
+  
+        day.classList.add('active-inp');
+        labDay.classList.add('active-lab');
+        errDay.classList.add('active-err');
+
+        resDay.innerText = '--';
+        resMonth.innerText = '--';
+        resYears.innerText = '--';
+
+        checkDay = false;
+      }
       
-    }  else if (resultYears.length !== 4 || resultYears == 0){
+      //check if month with 31 days and day's value are correct
+      if(((month.value == 4 || month.value == 6 || month.value == 9 || month.value == 11) && day.value > 30)) {
+        
+        day.classList.add('active-inp');
+        labDay.classList.add('active-lab');
+        errDay.classList.add('active-err');
+        errDay.innerText = 'Must be a valid day';
 
-      years.classList.add('active-inp');
-      labYears.classList.add('active-lab');
-      errYears.classList.add('active-err');
-      errYears.innerText = 'Must be a valid year'
+        resDay.innerText = '--';
+        resMonth.innerText = '--';
+        resYears.innerText = '--';
 
-      resDay.innerText = '--';
-      resMonth.innerText = '--';
-      resYears.innerText = '--';
-
-      //differents calc to result the correct age 
-    } else if (resultMonth > 0 && resultMonth <= currMonth && resultDay > 0 && resultDay <= currDay && resultYears > 0 && resultYears < currYears) {
-      resDay.innerText = currDay - resultDay;
-      resMonth.innerText = currMonth - resultMonth;
-      resYears.innerText = currYears - resultYears;
-
-    } else if (resultMonth > currMonth && resultDay < currDay) {
-      resYears.innerText = currYears - resultYears -1;
-      resMonth.innerText = (12 - (resultMonth - currMonth));
-      resDay.innerText = currDay - resultDay;
-
-    } else if (resultDay > currDay && resultMonth < currMonth) {
-      resYears.innerText = currYears - resultYears;
-      resMonth.innerText = (currMonth - resultMonth) - 1;
-      resDay.innerText = 30 - (resultDay - currDay);
-
-    } else if (resultDay > currDay && resultMonth > currMonth) {
-      resYears.innerText = currYears - resultYears -1;
-      resMonth.innerText = (12 - (resultMonth - currMonth)) -1;
-      resDay.innerText = 30 - (resultDay - currDay);
-
-    } else if (resultDay > currDay && resultMonth == currMonth) {
-      resYears.innerText = currYears - resultYears -1;
-      resMonth.innerText = 12 - (resultMonth - currMonth) - 1;
-      resDay.innerText = 30 - (resultDay - currDay);
-
-    } else if (resultDay == currDay && resultMonth > currMonth) {
-      resYears.innerText = currYears - resultYears -1;
-      resMonth.innerText = 12 - (resultMonth - currMonth);
-      resDay.innerText = resultDay - currDay;
-
-    } else {
-      day.classList.remove('active-inp');
-        labDay.classList.remove('active-lab');
-        errDay.classList.remove('active-err');
-
+        checkDay = false;
+      }
+      
+      
+      if ((years.value == currYears && month.value > currMonth) && (month.value.length < 3) ) {
+  
+        month.classList.add('active-inp');
+        labMonth.classList.add('active-lab');
+        errMonth.classList.add('active-err');
+        errMonth.innerText = 'Must be a in the past';
+        checkMonth = false;
+  
+        resYears.innerText = '--';
+        resDay.innerText = '--';
+        resMonth.innerText = '--';
+  
+       } else {
         month.classList.remove('active-inp');
-          labMonth.classList.remove('active-lab');
-          errMonth.classList.remove('active-err');
-           
-          years.classList.remove('active-inp');
-          labYears.classList.remove('active-lab');
-          errYears.classList.remove('active-err');
-    }
+        labMonth.classList.remove('active-lab');
+        errMonth.classList.remove('active-err');
+
+        checkMonth = true;
+       }
+  
+       if (years.value == currYears && month.value >= currMonth && day.value > currDay && day.value.length < 3 ) {
+  
+        day.classList.add('active-inp');
+        labDay.classList.add('active-lab');
+        errDay.classList.add('active-err');
+        errDay.innerText = 'Must be in the past';
+        checkDay = false
+  
+        resYears.innerText = '--';
+        resDay.innerText = '--';
+        resMonth.innerText = '--';
+       }
+  
+       //check if month's value are correct
+       if (month.value.length > 2) {
+        month.classList.add('active-inp');
+        labMonth.classList.add('active-lab');
+        errMonth.classList.add('active-err');
+        errMonth.innerText = 'Must be a valid month'
+
+        checkMonth = false
+  
+      } else if (month.value < 1){
+        month.classList.add('active-inp');
+        labMonth.classList.add('active-lab');
+        errMonth.classList.add('active-err');
+        errMonth.innerText = 'This field is required'
+
+        checkMonth = false
+      }
+  }) 
 }
+
+
+//to start checking input values
+checkInput();
+let checkDay;
+let checkMonth;
+let checkYears;
+
+//check if three input's value are total empty
+function isEmpty() {
+  if (day.value === '' && month.value === '' && years.value === '') {
+
+    day.classList.add('active-inp');
+    labDay.classList.add('active-lab');
+    errDay.classList.add('active-err');
+    errDay.innerText = 'This field is required';
+  
+    month.classList.add('active-inp');
+    labMonth.classList.add('active-lab');
+    errMonth.classList.add('active-err');
+    errMonth.innerText = 'This field is required'
+  
+    years.classList.add('active-inp');
+    labYears.classList.add('active-lab');
+    errYears.classList.add('active-err');
+    errYears.innerText = 'This field is required';
+  
+    resDay.innerText = '--';
+    resMonth.innerText = '--';
+    resYears.innerText = '--';
+   }
+}
+
+
+// to calculate age with user's input values
+function calcAge () {
+  let resultDay = day.value;
+  let resultMonth = month.value;
+  let resultYears = years.value;
+
+
+ if (resultMonth > 0 && resultMonth <= currMonth && resultDay > 0 && resultDay <= currDay && resultYears > 0 && resultYears < currYears) {
+    resDay.innerText = currDay - resultDay;
+    resMonth.innerText = currMonth - resultMonth;
+    resYears.innerText = currYears - resultYears;
+
+  }  else if (resultMonth > currMonth && resultDay < currDay) {
+    resYears.innerText = currYears - resultYears -1;
+    resMonth.innerText = (12 - (resultMonth - currMonth));
+    resDay.innerText = currDay - resultDay;
+
+  } else if (resultDay > currDay && resultMonth < currMonth && resultYears < currYears) {
+    resYears.innerText = currYears - resultYears;
+    resMonth.innerText = (currMonth - resultMonth) - 1;
+    resDay.innerText = 30 - (resultDay - currDay);
+
+  } else if (resultDay > currDay && resultMonth > currMonth) {
+    resYears.innerText = currYears - resultYears -1;
+    resMonth.innerText = (12 - (resultMonth - currMonth)) -1;
+    resDay.innerText = 30 - (resultDay - currDay);
+
+  } else if (resultDay > currDay && resultMonth >= currMonth) {
+    resYears.innerText = currYears - resultYears -1;
+    resMonth.innerText = 12 - (resultMonth - currMonth) - 1;
+    resDay.innerText = 30 - (resultDay - currDay);
+
+  } else if (resultDay == currDay && resultMonth > currMonth) {
+    resYears.innerText = currYears - resultYears -1;
+    resMonth.innerText = 12 - (resultMonth - currMonth);
+    resDay.innerText = resultDay - currDay;
+
+  } else if (resultYears == currYears && resultMonth == currMonth && resultDay <= currDay){
+    resYears.innerText = resultYears - currYears;
+    resMonth.innerText = resultMonth - currMonth;
+    resDay.innerText = currDay - resultDay;
+
+  } else if (resultYears == currYears && resultMonth < currMonth && resultDay < currDay) {
+    resYears.innerText = resultYears - currYears;
+    resMonth.innerText = currMonth - resultMonth;
+    resDay.innerText = currDay - resultDay;
+
+  } else if (resultYears == currYears && resultMonth < currMonth && resultDay > currDay) {
+    resYears.innerText = resultYears - currYears;
+    resMonth.innerText = (currMonth - resultMonth) - 1;
+    resDay.innerText = 30 - (resultDay - currDay);
+  }
+}
+
+
 
 //event listener to start function that calc age
 document.addEventListener('click', (e) => {
-  if (e.target === subBtn || e.target === arrow) {
-      calcAge();
-    }  
+  if ((e.target === subBtn || e.target === arrow) && (checkDay === true && checkMonth === true && checkYears === true)) {
+        calcAge();
+    } else {
+      isEmpty()
+    }
 });
